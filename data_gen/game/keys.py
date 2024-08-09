81cd4f59ad24619a64a21ebe5c4db50c565769fb
Tux <177398239+tux-type@users.noreply.github.com>
2024-08-09 20:34:15 +0100

    Add image and key data collection functions to game

--- data_gen/game/keys.py
+++ data_gen/game/keys.py
@@ -1,5 +1,4 @@
 import pyxel
 
 KEY_UP: int = pyxel.KEY_E
-KEY_LEFT: int = pyxel.KEY_S
 KEY_RIGHT: int = pyxel.KEY_F
--- data_gen/game/state.py
+++ data_gen/game/state.py
@@ -3,14 +3,22 @@ from elements import Player, Platform, Obstacle
 from keys import KEY_RIGHT, KEY_UP
 from collections import deque
 
+from datetime import datetime
+import json
+from pathlib import Path
+
 
 class BasicGame:
-    def __init__(self, w=45, h=30, fps=5):
+    def __init__(self, w=45, h=30, fps=5, collect_data=True):
+        self.collect_data = collect_data
+
         self.w = w
         self.h = h
-        pyxel.init(width=w, height=h, fps=fps)
+        pyxel.init(width=w, height=h, fps=fps, display_scale=1)
         pyxel.fullscreen(False)
 
+        self.update_counter = 0
+        self.sc_flag = False
         self.base_y_position = pyxel.floor(pyxel.height * (2 / 3))
         self.base_platform = Platform(
             x=0,
@@ -31,6 +39,18 @@ class BasicGame:
         self.obstacles = deque()
         # Next obstacle is a misleading name, can be obstacle behind if no more ahead
         self.next_obstacle = None
+
+        if collect_data:
+            data_dir_name = "collection_" + datetime.today().strftime("%Y%m%d-%H%M%S")
+
+            self.data_collection_dir = (
+                Path(__file__).parents[2] / "data" / "combined" / data_dir_name
+            )
+            # TODO: Replace with logger
+            print(f"Assuming project directory: {self.data_collection_dir.parents[2]}")
+            print(f"Creating directory: {self.data_collection_dir}")
+            self.data_collection_dir.mkdir(parents=False, exist_ok=False)
+            self.activated_keys = []
 
     def render_background(self, col_a=7, col_b=13):
         col_a, col_b = (col_a, col_b) if self.offset % 2 == 0 else (col_b, col_a)
@@ -70,9 +90,16 @@ class BasicGame:
                     return
 
     def update(self):
+        # Acts as an ID for key press and image data
+        if self.collect_data:
+            self.update_counter += 1
+            self.activated_keys = []
         self.update_next_obstacle()
 
         if pyxel.btnp(KEY_RIGHT, hold=0, repeat=1):
+            if self.collect_data:
+                self.activated_keys.append("KEY_RIGHT")
+
             if not self.obstacles:
                 self.offset += 1
             else:
@@ -95,11 +122,12 @@ class BasicGame:
 
         if pyxel.btn(KEY_UP):
             # TODO: Handle both keys pressed together better (UP AND RIGHT)
-            print("KEY_UP")
+            if self.collect_data:
+                self.activated_keys.append("KEY_UP")
             if not self.player.jumping and not self.player.falling:
                 self.player.jump()
 
-        # Check if need to fall
+        # Check if player needs to fall
         if self.next_obstacle:
             assert self.next_obstacle is not None, "next_obstacle should be set"
             is_player_above_obstacle = (
@@ -108,7 +136,6 @@ class BasicGame:
             )
             if is_player_above_obstacle:
                 self.player.surface_y = self.next_obstacle.y
-                print("Above obstacle")
             elif self.player.surface_y != self.base_platform.y:
                 self.player.surface_y = self.base_platform.y
                 self.player.falling = True
@@ -118,11 +145,43 @@ class BasicGame:
 
         self.player.update()
 
+        if self.collect_data:
+            self.save_screenshot()
+            self.save_pressed_keys()
+
         if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.screencast(scale=1)
             pyxel.quit()
 
+    def save_screenshot(self):
+        pyxel.screenshot(scale=1)
+
+        # pyxel only saves screenshots in Desktop
+        path = Path("~/Desktop").expanduser()
+        screenshots = list(path.glob("*.png"))
+        assert len(screenshots) == 1, "there should be only one pyxel png screenshot in save dir"
+        new_name = (
+            str(self.data_collection_dir)
+            + "/"
+            + screenshots[0].stem
+            + "_X"
+            + str(self.update_counter)
+            + screenshots[0].suffix
+        )
+        screenshots[0].rename(new_name)
+
+    def save_pressed_keys(self):
+        update_id = "X" + str(self.update_counter)
+        keys_with_id = {update_id: self.activated_keys}
+        keys_file = self.data_collection_dir / "key_activations.json"
+        if keys_file.exists():
+            with open(keys_file, "a") as f_out:
+                json.dump(keys_with_id, f_out)
+        else:
+            with open(keys_file, "w") as f_out:
+                json.dump(keys_with_id, f_out)
+
     def draw(self):
-        # pyxel.cls(0)
         self.render_background()
         self.base_platform.render(col=15)
         self.player.render()
