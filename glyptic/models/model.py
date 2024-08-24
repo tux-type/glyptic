bef24911cea044afc6ba22217c007a755d7155bc
Tux <177398239+tux-type@users.noreply.github.com>
2024-08-24 18:19:01 +0100

    Tidy up training and sampling code

--- glyptic/models/model.py
+++ glyptic/models/model.py
@@ -1,18 +1,5 @@
-from data.data_loader import FrameKeyDataLoader
-from glyptic.models.train import load_data
-from jax import grad, random, vmap, jit
+from jax import jit, random, vmap
 import jax.numpy as jnp
-from jax.scipy.special import logsumexp
-
-import time
-
-
-# input_images = load_data()
-#
-# input_images.shape
-#
-# # Do not reshape for real network - to use conv
-# new_input_images = input_images.reshape(input_images.shape[0], -1)
 
 
 def random_layer_params(m, n, key, scale=1e-2):
@@ -25,46 +12,13 @@ def init_network_params(sizes, key):
     return [random_layer_params(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
 
 
-@jit
-def linear_diffusion_schedule(time_steps):
-    min_rate = 0.0001
-    max_rate = 0.02
-    betas = min_rate + time_steps * (max_rate - min_rate)
-    alphas = 1 - betas
-    alpha_bars = jnp.cumprod(alphas)
-    signal_rates = alpha_bars
-    noise_rates = 1 - alpha_bars
-    return noise_rates, signal_rates
-
-
-@jit
-def cosine_diffusion_schedule(time_steps):
-    signal_rates = jnp.cos(time_steps * (jnp.pi / 2))
-    noise_rates = jnp.sin(time_steps * (jnp.pi / 2))
-    return noise_rates, signal_rates
-
-
-@jit
-def offset_cosine_diffusion_schedule(time_steps):
-    min_signal_rate = 0.02
-    max_signal_rate = 0.95
-    start_angle = jnp.acos(max_signal_rate)
-    end_angle = jnp.acos(min_signal_rate)
-
-    diffusion_angles = start_angle + time_steps * (end_angle - start_angle)
-
-    signal_rates = jnp.cos(diffusion_angles)
-    noise_rates = jnp.sin(diffusion_angles)
-    return noise_rates, signal_rates
-
-
 def relu(z):
     return jnp.maximum(0, z)
 
 
 # TODO: Revise predict func, esp. output values (currently probs)
 @jit
-def predict(params, x):
+def forward(params, x):
     a = x
     for w, b in params[:-1]:
         z = jnp.dot(w, a) + b
@@ -76,88 +30,5 @@ def predict(params, x):
 
 
 @jit
-def batch_predict(params, batched_x):
-    # return vmap(predict, in_axes=(None, 0))(params, batched_x)
-    return vmap(predict)(params, batched_x)
-
-
-@jit
-def denoise(params, noisy_image, noise_rate, signal_rate):
-    # TODO:
-    # - find out why square the noise rates - to have smaller steps for denoising than noising?
-    # - figure out the more optimal method (perhaps array concat more appropriate)
-    pred_noise = predict(params, noisy_image)
-    # REAL IMPLEMENTATION:
-    # pred_noise = predict(params, [noisy_image, noise_rate**2])
-    # Remove noise scaled by number of noise steps and scale back up by signal rates to overall same
-    # intensity
-    pred_image = (noisy_image - noise_rate * pred_noise) / signal_rate
-    return pred_noise, pred_image
-
-
-@jit
-def batch_denoise(params, batch_noisy_image, batch_noise_rate, batch_signal_rate):
-    # in_axes is making sure that params are not included in batching??
-    return vmap(denoise, in_axes=(None, 0, 0, 0))(
-        params, batch_noisy_image, batch_noise_rate, batch_signal_rate
-    )
-
-
-@jit
-def mean_absolute_error(y, preds):
-    return jnp.mean(jnp.abs(y - preds))
-
-
-# @jit
-# def batch_mean_absolute_error(batch_y, batch_preds):
-#     return vmap(mean_absolute_error)(batch_y, batch_preds)
-
-
-# MAE
-@jit
-def loss(params, batch_x, batch_noise_rate, batch_signal_rate, batch_noise):
-    batch_noise_preds, batch_image_preds = batch_denoise(
-        params, batch_x, batch_noise_rate, batch_signal_rate
-    )
-    return mean_absolute_error(batch_noise_preds, batch_noise)
-
-
-# TODO: @jit
-def update(params, batch_x, batch_y):
-    # TODO: Figure out a good way to track keys - perhaps split
-    noises = random.normal(random.key(1), shape=batch_x.shape)
-    learning_rate = 0.01
-    batch_size = batch_x.shape[0]
-    time_steps = random.uniform(random.key(2), shape=batch_size, minval=0.0, maxval=1.0)
-    batch_noise_rate, batch_signal_rate = cosine_diffusion_schedule(time_steps)
-    loss_i = loss(params, batch_x, batch_noise_rate, batch_signal_rate, noises)
-    print(f"loss: {loss_i}")
-    grads = grad(loss)(params, batch_x, batch_noise_rate, batch_signal_rate, noises)
-    return [
-        (w - learning_rate * dw, b - learning_rate * db) for (w, b), (dw, db) in zip(params, grads)
-    ]
-
-
-# Input layer size flattened - determine if suitable
-layer_sizes = [4050, 512, 512, 4050]
-batch_size = 128
-params = init_network_params(layer_sizes, random.key(0))
-
-data_loader = FrameKeyDataLoader(
-    "data/combined/collection_20240813-195533/", batch_size=128, shuffle=False
-)
-
-
-num_epochs = 5
-for epoch in range(num_epochs):
-    start_time = time.time()
-    for x, y in data_loader:
-        # TODO: Remove reshape - Not needed when using conv later
-        params = update(params, x.reshape(x.shape[0], -1), y)
-    epoch_time = time.time() - start_time
-
-    # train_loss = loss(params, train_images, train_labels)
-    # test_loss = loss(params, test_images, test_labels)
-    print(f"Epoch {epoch} in {epoch_time:0.2f} sec")
-    # print(f"Training set accuracy {train_loss}")
-    # print(f"Test set accuracy {test_loss}")
+def batch_forward(params, batched_x):
+    return vmap(forward, in_axes=(None, 0))(params, batched_x)
--- glyptic/models/train.py
+++ glyptic/models/train.py
@@ -1,10 +1,15 @@
 from glyptic.data import FrameKeyDataLoader
 import jax.numpy as jnp
-from jax import Array, jit, vmap
+from jax import Array, jit, vmap, random, grad
+from .model import init_network_params
+from .sampling import batch_denoise, cosine_diffusion_schedule
+
+import time
 
 
+@jit
 def preprocess(image: Array):
-    image = image.astype("float32") / 255.0
+    image = image.astype(jnp.float32) / 255.0
     return image
 
 
@@ -15,7 +20,7 @@ def batched_preprocess(batched_images: Array):
 
 def load_data() -> Array:
     data_loader = FrameKeyDataLoader(
-        "data/combined/collection_20240813-195533/", batch_size=1, shuffle=False
+        "data/combined/collection_20240813-195533/", batch_size=128, shuffle=False
     )
 
     input_images = []
@@ -31,8 +36,62 @@ def load_data() -> Array:
     return input_images
 
 
+@jit
+def mean_absolute_error(y, preds):
+    return jnp.mean(jnp.abs(y - preds))
+
+
+# MAE
+@jit
+def loss(params, batch_x, batch_noise_rate, batch_signal_rate, batch_noise):
+    batch_noise_preds, batch_image_preds = batch_denoise(
+        params, batch_x, batch_noise_rate, batch_signal_rate
+    )
+    return mean_absolute_error(batch_noise_preds, batch_noise)
+
+
+# TODO: @jit
+def update(params, batch_x, batch_y, key):
+    key_noise, key_time_step = random.split(key)
+    noises = random.normal(key_noise, shape=batch_x.shape)
+    learning_rate = 0.01
+    batch_size = batch_x.shape[0]
+    time_steps = random.uniform(key_time_step, shape=batch_size, minval=0.0, maxval=1.0)
+    batch_noise_rate, batch_signal_rate = cosine_diffusion_schedule(time_steps)
+    loss_i = loss(params, batch_x, batch_noise_rate, batch_signal_rate, noises)
+    print(f"loss: {loss_i}")
+    grads = grad(loss)(params, batch_x, batch_noise_rate, batch_signal_rate, noises)
+    return [
+        (w - learning_rate * dw, b - learning_rate * db) for (w, b), (dw, db) in zip(params, grads)
+    ]
+
+
 def train_model():
-    pass
+    # Input layer size flattened - determine if suitable
+    layer_sizes = [4050, 512, 512, 4050]
+    batch_size = 128
+    params_key, update_key = random.split(random.key(0))
+    params = init_network_params(layer_sizes, params_key)
+
+    data_loader = FrameKeyDataLoader(
+        "data/combined/collection_20240813-195533/", batch_size=batch_size, shuffle=False
+    )
+
+    num_epochs = 5
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        for x, y in data_loader:
+            x = batched_preprocess(x)
+            update_key, batch_update_key = random.split(update_key)
+            # TODO: Remove reshape - Not needed when using conv later
+            params = update(params, x.reshape(x.shape[0], -1), y, batch_update_key)
+        epoch_time = time.time() - start_time
+
+        # train_loss = loss(params, train_images, train_labels)
+        # test_loss = loss(params, test_images, test_labels)
+        print(f"Epoch {epoch} in {epoch_time:0.2f} sec")
+        # print(f"Training set accuracy {train_loss}")
+        # print(f"Test set accuracy {test_loss}")
 
 
 if __name__ == "__main__":
