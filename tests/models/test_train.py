560c8607c227f4c8a8d4d19126e5e6ad850f1115
Tux <177398239+tux-type@users.noreply.github.com>
2024-09-13 19:53:35 +0100

    Move data_dir to config

--- tests/models/test_train.py
+++ tests/models/test_train.py
@@ -6,12 +6,19 @@ from glyptic.models import get_config, train_and_evaluate
 
 
 @pytest.fixture(scope="module")
-def data_dir():
-    return str(Path(__file__).parent.parent) + "/dataset/collection_20240908-153517"
+def train_data_dir():
+    return str(Path(__file__).parent.parent) + "/dataset/training/collection_20240908-153517"
 
 
-def test_train_and_evaluate(data_dir):
+@pytest.fixture(scope="module")
+def val_data_dir():
+    return str(Path(__file__).parent.parent) + "/dataset/validation/collection_20240908-153517"
+
+
+def test_train_and_evaluate(train_data_dir: str, val_data_dir: str):
     config = get_config()
     config["batch_size"] = 32
     config["num_epochs"] = 1
-    train_and_evaluate(config=config, data_dir=data_dir)
+    config["train_data_dir"] = train_data_dir
+    config["val_data_dir"] = val_data_dir
+    train_and_evaluate(config=config)
