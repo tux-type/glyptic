d0b4c4b77c3c0b958e86759b0bcf70e14803d6ea
Tux <177398239+tux-type@users.noreply.github.com>
2024-09-13 19:53:06 +0100

    Add eval step as part of training using validation data

--- glyptic/models/train.py
+++ glyptic/models/train.py
@@ -37,13 +37,19 @@ def preprocess(image: Array):
 
 @jit
 def apply_model(state: TrainState, xt_batch: Array, times_batch: Array, epsilon_batch: Array):
-    def loss_fn(params):
-        epsilon_theta = state.apply_fn({"params": params}, xt_batch, times_batch)
-        loss = optax.l2_loss(epsilon_theta, epsilon_batch)
-        return jnp.mean(loss)
-
-    loss, grads = value_and_grad(loss_fn)(state.params)
+    grad_function = value_and_grad(loss_fn, argnums=0)
+    loss, grads = grad_function(state.params, state, xt_batch, times_batch, epsilon_batch)
     return loss, grads
+
+
+@jit
+# TODO: Determine if need to pass params separately from state for any reason
+def loss_fn(
+    params: Array, state: TrainState, xt_batch: Array, times_batch: Array, epsilon_batch: Array
+):
+    epsilon_theta = state.apply_fn({"params": params}, xt_batch, times_batch)
+    loss = optax.l2_loss(epsilon_theta, epsilon_batch)
+    return jnp.mean(loss)
 
 
 def train_step(state: TrainState, x0_batch: Array, config: dict[str, Any], rng: Array):
@@ -64,21 +70,58 @@ def train_step(state: TrainState, x0_batch: Array, config: dict[str, Any], rng: 
     return state, loss
 
 
-def train_and_evaluate(config: dict[str, Any], data_dir: str):
+# TODO: Add loss calculation
+def eval_step(state: TrainState, x0_batch: Array, config: dict[str, Any], rng: Array):
+    times_rng, noise_rng = random.split(rng)
+    noise_schedule = create_noise_schedule(num_steps=config["num_steps"])
+    batch_size = x0_batch.shape[0]
+    times_batch = random.randint(
+        times_rng, shape=(batch_size,), minval=0, maxval=config["num_steps"]
+    )
+    noise_batch = random.normal(noise_rng, shape=x0_batch.shape)
+    xt_batch = q_sample(
+        alpha_bar=noise_schedule["alpha_bar"], x0=x0_batch, times=times_batch, epsilon=noise_batch
+    )
+    loss = loss_fn(
+        params=state.params,
+        state=state,
+        xt_batch=xt_batch,
+        times_batch=times_batch,
+        epsilon_batch=noise_batch,
+    )
+    return loss
+
+
+def train_and_evaluate(config: dict[str, Any]):
     rng = random.key(config["rng_seed"])
 
-    rng, init_rng = random.split(rng)
+    rng, init_rng, eval_rng = random.split(rng, num=3)
     state = create_train_state(rng=init_rng, config=config)
 
-    data_loader = FrameKeyDataLoader(data_dir, batch_size=config["batch_size"], shuffle=False)
+    # Decide how to change data as Array and move it to GPU efficiently
+    data_loader = FrameKeyDataLoader(
+        config["train_data_dir"], batch_size=config["batch_size"], shuffle=False
+    )
+    validation_data_loader = FrameKeyDataLoader(
+        config["val_data_dir"], batch_size=config["batch_size"], shuffle=False
+    )
 
-    # i = 0
     for epoch in range(1, config["num_epochs"] + 1):
+        training_loss = []
         for x, y in data_loader:
             rng, batch_rng = random.split(rng)
-            x = vmap(preprocess)(x)
+            x = vmap(preprocess)(jnp.array(x))
             state, loss = train_step(state, x0_batch=x, config=config, rng=batch_rng)
-            print(loss)
-            # if i % 100 == 0:
-            #     print(loss)
-            # i += 1
+            training_loss.append(loss)
+        # TODO: Determine if better to reuse RNG for more consistent evals across epochs
+        # TODO: Check if better to accumulate and average rather than calculate across whole data set
+        # train_set_loss = eval_step(
+        #     state, x0_batch=jnp.array(data_loader.all_images[0]), config=config, rng=eval_rng
+        # )
+        val_set_loss = eval_step(
+            state,
+            x0_batch=jnp.array(validation_data_loader.all_images[0]),
+            config=config,
+            rng=eval_rng,
+        )
+        print(jnp.array(training_loss).mean(), val_set_loss)
