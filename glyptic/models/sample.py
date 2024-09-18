a146c445bf6137d2d49476e69d46045dee1eaf38
Tux <177398239+tux-type@users.noreply.github.com>
2024-09-18 18:19:14 +0100

    Reorganise imports in sample

--- glyptic/models/sample.py
+++ glyptic/models/sample.py
@@ -1,8 +1,7 @@
 from typing import Mapping
 
 from flax.core import FrozenDict, freeze
-from jax import Array, random
-from jax import jit
+import jax
 import jax.numpy as jnp
 
 
@@ -16,32 +15,32 @@ def create_noise_schedule(num_steps: int) -> FrozenDict:
     )
 
 
-@jit
-def q_xt_x0(alpha_bar: Array, x0: Array, times: Array) -> tuple[Array, Array]:
-    # Signal rates at different times steps
+@jax.jit
+def q_xt_x0(alpha_bar: jax.Array, x0: jax.Array, times: jax.Array) -> tuple[jax.Array, jax.Array]:
+    # Signal rates at different time steps
     alpha_bar = alpha_bar[times].reshape(-1, 1, 1, 1)
     mean = alpha_bar**0.5 * x0
     variance = 1 - alpha_bar
     return mean, variance
 
 
-@jit
-def q_sample(alpha_bar, x0: Array, times: Array, epsilon: Array):
+@jax.jit
+def q_sample(alpha_bar, x0: jax.Array, times: jax.Array, epsilon: jax.Array):
     mean, variance = q_xt_x0(alpha_bar=alpha_bar, x0=x0, times=times)
     return mean + (variance**0.5) * epsilon
 
 
 def p_sample(
-    rng: Array,
-    noise_schedule: Mapping[str, Array],
+    rng: jax.Array,
+    noise_schedule: Mapping[str, jax.Array],
     epsilon_theta,
-    xt: Array,
-    times: Array,
+    xt: jax.Array,
+    times: jax.Array,
 ):
     alpha_bar = noise_schedule["alpha_bar"][times].reshape(-1, 1, 1, 1)
     alpha = noise_schedule["alpha"][times].reshape(-1, 1, 1, 1)
     epsilon_coef = (1 - alpha) / (1 - alpha_bar) ** 0.5
     mean = 1 / (alpha**0.5) * (xt - epsilon_coef - epsilon_theta)
     variance = noise_schedule["sigma2"][times].reshape(-1, 1, 1, 1)
-    epsilon = random.normal(rng, shape=xt.shape)
+    epsilon = jax.random.normal(rng, shape=xt.shape)
     return mean + (variance**0.5) * epsilon
